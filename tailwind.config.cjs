d4e0a9421aea549e7338e34b4df95f6796dfaffb
Ramless55 <ramiromoraleskraff@gmail.com>
2023-09-23 13:29:57 -0300

    first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+import daisyui from "daisyui"
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {},
+    fontFamily: {
+      chakra: ['Chakra Petch']
+    },
+  },
+  plugins: [
+    daisyui
+  ]
+}

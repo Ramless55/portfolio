1c191425f4581b51de935d45687d2e22f7c24e84
Ramless55 <ramiromoraleskraff@gmail.com>
2023-09-23 18:01:09 -0300

    color issues corrected

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,15 @@ import daisyui from "daisyui"
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'main': '#1d232a',
+        'pink': '#D926A9',
+        "primary": "#5616C5",
+        'neutral-focus': '#242B33'
+      }
+    },
+
     fontFamily: {
       chakra: ['Chakra Petch']
     },
